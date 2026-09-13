df20cea7630602ca13143ad4da08f9e4763c0b21
essde342-bit <essde342@gmail.com>
2026-09-13 18:53:32 +0300

    Update MindazMod.java

--- src/main/java/com/example/mindazmod/MindazMod.java
+++ src/main/java/com/example/mindazmod/MindazMod.java
@@ -1,23 +1,24 @@
 package com.example.mindazmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class MindazMod implements ModInitializer {
     public static final String MOD_ID = "mindazmod";
 
     public static final Block CLOUD_BLOCK = new CloudBlock();
-    public static final Item MINDAZ_SWORD = new MindazSwordItem(new Item.Settings().group(ItemGroup.COMBAT));
+    public static final Item MINDAZ_SWORD = new MindazSwordItem(new FabricItemSettings());
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "cloud"), CLOUD_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "cloud"), new BlockItem(CLOUD_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "mindaz"), MINDAZ_SWORD);
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "cloud"), CLOUD_BLOCK);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "cloud"), new BlockItem(CLOUD_BLOCK, new FabricItemSettings()));
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "mindaz"), MINDAZ_SWORD);
     }
 }
