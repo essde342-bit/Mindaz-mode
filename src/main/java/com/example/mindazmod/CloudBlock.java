6f0af141b0549b45ba4ea68c909875117cb4d65d
essde342-bit <essde342@gmail.com>
2026-09-13 18:58:28 +0300

    Update CloudBlock.java

--- src/main/java/com/example/mindazmod/CloudBlock.java
+++ src/main/java/com/example/mindazmod/CloudBlock.java
@@ -2,10 +2,10 @@ package com.example.mindazmod;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
 
 public class CloudBlock extends Block {
     public CloudBlock() {
-        super(FabricBlockSettings.copyOf(Blocks.WHITE_WOOL).strength(0.3f).nonOpaque());
+        super(FabricBlockSettings.of(Material.WOOL).strength(0.3f).nonOpaque());
     }
 }
