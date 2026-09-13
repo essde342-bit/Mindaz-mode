0679e04a05e787724c79344d985142a333e57884
essde342-bit <essde342@gmail.com>
2026-09-13 18:57:37 +0300

    Update MindazSwordItem.java

--- src/main/java/com/example/mindazmod/MindazSwordItem.java
+++ src/main/java/com/example/mindazmod/MindazSwordItem.java
@@ -1,10 +1,11 @@
 package com.example.mindazmod;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterials;
 
 public class MindazSwordItem extends SwordItem {
-    public MindazSwordItem(Settings settings) {
+    public MindazSwordItem(Item.Settings settings) {
         super(ToolMaterials.NETHERITE, 3, -2.4f, settings);
     }
 }
